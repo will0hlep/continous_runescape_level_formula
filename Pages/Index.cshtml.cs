88e23fc7b8363d2582009c16e0dc07dca924b007
will0hlep <williamrpaine@gmail.com>
2026-07-09 23:15:06 +0100

    web app initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RSConLvl.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
